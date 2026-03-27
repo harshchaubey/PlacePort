0e2a7ca44579038dc3b1e723ffab89f5d863a65f
harshchaubey <harshchaubey189@gmail.com>
2026-03-27 20:28:13 +0530

    Update ApplicationController.java

--- backend/src/main/java/com/placement/portal/controller/ApplicationController.java
+++ backend/src/main/java/com/placement/portal/controller/ApplicationController.java
@@ -9,8 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import com.placement.portal.service.CloudinaryService; 
-import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
