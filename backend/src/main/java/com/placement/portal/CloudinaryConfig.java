c9cfd9cd460ad2c1624daa77d955c9f4f5786b28
harshchaubey <harshchaubey189@gmail.com>
2026-03-27 14:32:20 +0530

    Add Cloudinary configuration class

---
+++ backend/src/main/java/com/placement/portal/CloudinaryConfig.java
@@ -0,0 +1,21 @@
+@Configuration
+public class CloudinaryConfig {
+
+    @Value("${CLOUDINARY_NAME}")
+    private String cloudName;
+
+    @Value("${CLOUDINARY_API_KEY}")
+    private String apiKey;
+
+    @Value("${CLOUDINARY_API_SECRET}")
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret,
+                "secure", true));
+    }
+}
